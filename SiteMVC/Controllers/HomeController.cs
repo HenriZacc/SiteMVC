db738f73b61ac4ef4e5be24d89e2c60166665abb
HenriZacc <HenriR.Zaccaro@hotmail.com>
2022-11-29 11:35:33 -0300

    first commit

---
+++ SiteMVC/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SiteMVC.Models;
+
+namespace SiteMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home= new HomeModel();
+
+            home.Nome = "Henri Rodrigues";
+            home.Email = "henrir.zaccaro@hotmail.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
